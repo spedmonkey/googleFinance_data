4226b147f7235d2c33b17b73902e716229dc78bc
spedmonkey <spedcr@gmail.com>
2017-06-17 21:59:59 +1200

    adding functions:
    calcStrikPrice()
    sepCallPuts()

--- optionData.py
+++ optionData.py
@@ -5,36 +5,59 @@ from datetime import date, timedelta
 from pandas_datareader.data import Options
 import datetime
 
+class optionsData(object):
+    def __init__(self):
+        stockPrice = self.getStockPrice("AAPL")
+        df = self.getOptionsData("AAPL")
+        df = self.calcStrikePrice(stockPrice, df)
+        df = self.sepCallPuts(df)
+        self.writeDataFrame(df)
+    #Function to get stock price from google finance
+    def getStockPrice(self,stock):
+        start = date.today() - timedelta(7)
+        end= datetime.datetime.today().strftime('%Y-%m-%d')
+        aapl = data.DataReader(stock, "google", start, end)
+        stockPrice = aapl.iloc[[-1]]['Close'].values[0]
+        return stockPrice
 
-#Function to get stock price from google finance
-def getStockPrice(stock):
-    start = date.today() - timedelta(7)
-    end= datetime.datetime.today().strftime('%Y-%m-%d')
+    #Get option data from yahoo finance
+    def getOptionsData(self,optionIndex):
+        optionIndex = Options(optionIndex, 'yahoo')
 
-    aapl = data.DataReader(stock, "google", start, end)
+        data = optionIndex.get_all_data()
+        data = data.reset_index()
 
-    stockPrice = aapl.iloc[[-1]]['Close'].values[0]
-    return stockPrice
-
-
-#Get option data from yahoo finance
-def getOptionsData(optionIndex):
-    optionIndex = Options(optionIndex, 'yahoo')
-    data = optionIndex.get_all_data()
-    data = data.reset_index()
-    data= data.sort_values(['Expiry'], ascending=[False])
-    return data
-
-
-#Write Data to excel spreadsheet
-def writeDataFrame(df):
-    writer = pd.ExcelWriter(
-        'C:/Users/sped\PycharmProjects/cboeData/output.xlsx')
-    df.to_excel(writer, 'Sheet1')
-    writer.save()
-    print 'data written'
+        data= data.sort_values(['Expiry'], ascending=[True])
+        data = data.reset_index()
 
 
-stockPrice = getStockPrice("AAPL")
-df = getOptionsData("AAPL")
-writeDataFrame(df)
+        data=data.drop(['index'], axis=1)
+
+        print list(data)
+        return data
+
+    #Calculate whether strike price is within 0.15%+- of stock price
+    def calcStrikePrice(self,stockPrice, df):
+        for index, row in df.iterrows():
+            if not stockPrice-0.15*stockPrice < row['Strike']<1.15*stockPrice:
+                df = df.drop([index], axis=0)
+        return df
+
+    #Divide Call and Puts
+    def sepCallPuts(self,df):
+        for index, row in df.iterrows():
+            if df['Type'][index] == 'call':
+                print df['Type'][index]
+                df=df.drop([index], axis=0)
+        return df
+
+    #Write Data to excel spreadsheet
+    def writeDataFrame(self,df):
+        writer = pd.ExcelWriter(
+            'C:/Users/sped\PycharmProjects/cboeData/output.xlsx')
+        df.to_excel(writer, 'Sheet1')
+        writer.save()
+        print 'data written'
+
+
+a=optionsData()
